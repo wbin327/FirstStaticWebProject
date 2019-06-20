8806b23ad071d163f42e2dc4ced7ca3e72ae47da
wbin <33045209+wbin327@users.noreply.github.com>
2019-06-20 11:51:51 +0800

    修改：完成列表页和主页之间的联动

--- static/component/right-sidebar/right-sidebar.js
+++ static/component/right-sidebar/right-sidebar.js
@@ -1,30 +1,52 @@
 // 文件路径
-let baseUrl = '/FirstStaticWebProject/static/component/right-sidebar/'
+let baseUrl = '/FirstStaticWebProject/'
+// let baseUrl = '/'
+
+// 格式化字符串
+String.prototype.format = function(args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+    var data = arguments;
+    if (arguments.length == 1 && typeof (args) == "object") {
+        data = args;
+    }
+    for (var key in data) {
+        var value = data[key];
+        if (undefined != value) {
+            // result = result.replace("{" + key + "}", value);
+            // 替换字符串中匹配到的所有字符
+            result = result.replace(new RegExp("{" + key + "}", 'g'), value);
+        }
+    }
+    return result;
+}
 
 // 导航栏html
 let sidebar_html = '<div class="right-sidebar-ico" id="right-sidebar-ico">\n' +
-    '    <img src="/FirstStaticWebProject/static/component/right-sidebar/images/homeico.png">\n' +
+    '    <img src="{baseUrl}/static/component/right-sidebar/images/homeico.png">\n' +
     '</div>\n' +
     '<div class="right-sidebar" style="display: none;">\n' +
     '    <div class="main-sidebar">\n' +
     '        <div class="sidebar-close" id="sidebar-close">\n' +
-    '            <img src="/FirstStaticWebProject/static/component/right-sidebar/images/closeico.png">\n' +
+    '            <img src="{baseUrl}/static/component/right-sidebar/images/closeico.png">\n' +
     '        </div>\n' +
     '        <div class="sidebar-logo">\n' +
-    '            <img src="/FirstStaticWebProject/static/component/right-sidebar/images/right-sidebar-logo.png">\n' +
+    '            <img src="{baseUrl}/static/component/right-sidebar/images/right-sidebar-logo.png">\n' +
     '        </div>\n' +
-    '        <div class="main-sidebar-button">\n' +
-    '            项目背景\n' +
-    '        </div>\n' +
-    '        <div class="main-sidebar-button">\n' +
-    '            设计策略\n' +
-    '        </div>\n' +
-    '        <div class="main-sidebar-button">\n' +
-    '            设计成果\n' +
-    '        </div>\n' +
-    '        <div class="main-sidebar-button">\n' +
-    '            产品图纸\n' +
-    '        </div>\n' +
+//    '        <div class="main-sidebar-button">\n' +
+    '            <a href="{baseUrl}/list-page/list-page-project.html" class="main-sidebar-button">项目背景</a>\n' +
+//    '        </div>\n' +
+//     '        <div class="main-sidebar-button">\n' +
+    '            <a href="{baseUrl}/list-page/list-page-strategy.html" class="main-sidebar-button">设计策略</a>\n' +
+//    '        </div>\n' +
+//    '        <div class="main-sidebar-button">\n' +
+    '            <a href="{baseUrl}/list-page/list-page-presentation1.html" class="main-sidebar-button">设计成果</a>\n' +
+//    '        </div>\n' +
+//    '        <div class="main-sidebar-button">\n' +
+    '            <a href="{baseUrl}/list-page/list-page-product1.html" class="main-sidebar-button">产品图纸</a>\n' +
+//    '        </div>\n' +
     '    </div>\n' +
     '    <div class="secondary-sidebar">\n' +
     '        <div class="secondary-button-group">\n' +
@@ -125,7 +147,11 @@ let sidebar_html = '<div class="right-sidebar-ico" id="right-sidebar-ico">\n' +
     '            </div>\n' +
     '        </div>\n' +
     '    </div>\n' +
-    '</div>'
+    '</div>';
+
+console.log("格式化前：" + sidebar_html);
+sidebar_html = sidebar_html.format({'baseUrl': baseUrl});
+console.log("格式化后：" + sidebar_html);
 
 // 为页面添加侧边栏,只需要在页面中声明<div class="right-sidebar" id="right-sidebar></div>,自动往该标签中添加元素
 $(".right-sidebar-component").html(sidebar_html);
@@ -137,7 +163,7 @@ css.attr({
     rel: "stylesheet",
     type: "text/css",
     // href: "/static/component/right-sidebar/css/right-sidebar.css",
-    href: "/FirstStaticWebProject/static/component/right-sidebar/css/right-sidebar.css",
+    href: "{baseUrl}/static/component/right-sidebar/css/right-sidebar.css".format({'baseUrl': baseUrl}),
 });
 
 // 侧边栏相关JS
---
+++ static/js/list-page.js
@@ -0,0 +1,4 @@
+$("header img").click(function () {
+    window.location.href = "/FirstStaticWebProject/";
+    // window.location.href = "/";
+})
