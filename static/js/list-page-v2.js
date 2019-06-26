1ad70868df4226ae3c7629acc037ffb5736fa795
wbin <33045209+wbin327@users.noreply.github.com>
2019-06-26 15:28:47 +0800

    新增：列表页V2版本

--- static/js/list-page-v2.js
+++ static/js/list-page-v2.js
@@ -1,8 +1,26 @@
-$(".main-box img").hover(show_black_screen, hide_black_screen);
-
+// $(document).ready(function () {
+//     // 往image-title中添加文件名
+//     edit_image_title();
+// })
+$('.main-box').hover(show_black_screen, hide_black_screen)
 function show_black_screen() {
-    $('.black-screen').show();
+    // $('.black-screen').show();
+    $('.black-screen').css({'visibility': 'visible'});
 }
 function hide_black_screen() {
-    $('.black-screen').hide();
+    // $('.black-screen').hide();
+    $('.black-screen').css({'visibility': 'hidden'});
 }
+
+// 小图鼠标移入移出效果
+$(".sub-box").hover(
+    function (e) {
+        // let target = $(e.target);
+        let target = e.currentTarget;
+        // debugger;
+        target.children[1].style.visibility = 'visible';
+    },
+    function (e) {
+        let target = e.currentTarget;
+        target.children[1].style.visibility = 'hidden';
+    })
