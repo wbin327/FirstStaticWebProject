c1a614abe2e70fe718a7b8acba6b09e9e1fd2229
wbin <33045209+wbin327@users.noreply.github.com>
2019-07-11 11:43:54 +0800

    修改：修改项目名称后，一同修改config.js中的项目请求根路径

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,8 @@
 // 项目根路径
-let baseUrl = '/FirstStaticWebProject/'
+// let baseUrl = '/FirstStaticWebProject/'
 // let baseUrl = '/'
 // let baseUrl = 'http://114.215.188.153:8100/'
+let baseUrl = '/GolfGardenProject/'
 
 // 详情页图片请求路径配置
 let details_page_images_path = baseUrl + '/static/images/details-page-images/'
